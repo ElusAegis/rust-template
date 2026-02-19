1317f90bc983e123fc817d2c590c5bf121160fe2
github-action-benchmark <github@users.noreply.github.com>
2026-02-19 21:25:14 +0000

    add Rust Template Benchmark (cargo) benchmark result for 888640c2234394bcb97130d51f468a7b28d7a540

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1771536314071,
+  "repoUrl": "https://github.com/NethermindEth/rust-template",
+  "entries": {
+    "Rust Template Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "wisemrmusa@gmail.com",
+            "name": "Musa AbdulKareem",
+            "username": "WiseMrMusa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "888640c2234394bcb97130d51f468a7b28d7a540",
+          "message": "ci: remove external data JSON path from benchmark.yml (#44)",
+          "timestamp": "2026-02-19T22:13:19+01:00",
+          "tree_id": "7f68a0e85909c74c5fedba162a40142cb58117a6",
+          "url": "https://github.com/NethermindEth/rust-template/commit/888640c2234394bcb97130d51f468a7b28d7a540"
+        },
+        "date": 1771536313106,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "add_small_integers valid",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "add_small_integers bound check",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sub_small_integers valid",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sub_small_integers bound check",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
